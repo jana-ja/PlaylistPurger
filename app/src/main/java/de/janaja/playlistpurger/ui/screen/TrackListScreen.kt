d2651fd4f1591d1c5352ca6e8384741734b13cce
jana-ja <jana.nein+account@posteo.de>
2025-04-05 15:32:28 +0200

    test for continuous recompositions -> doesnt happen, good

--- app/src/main/java/de/janaja/playlistpurger/ui/screen/TrackListScreen.kt
+++ app/src/main/java/de/janaja/playlistpurger/ui/screen/TrackListScreen.kt
@@ -1,24 +1,33 @@
 package de.janaja.playlistpurger.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alexstyl.swipeablecard.rememberSwipeableCardState
 import de.janaja.playlistpurger.ui.component.SwipeCard
+import de.janaja.playlistpurger.ui.component.SwipeCardState
 import de.janaja.playlistpurger.ui.component.SwipeDirection
 import de.janaja.playlistpurger.ui.component.TrackCard
 import de.janaja.playlistpurger.ui.component.rememberSwipeCardState
 import de.janaja.playlistpurger.ui.viewmodel.TrackListViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -32,6 +41,17 @@ fun TrackListScreen(
         emptyList()
     )
 
+    var topSwipeCardState: SwipeCardState? = null
+    val scope = rememberCoroutineScope()
+
+    var swipeStates = swipeableTracks
+        .map {
+            Log.d(TAG, "TrackListScreen: mappi in screen")
+            it to rememberSwipeableCardState()
+        }
+        .onEach { he ->
+            Log.d(TAG, "unvotedTracks: updatet ${he.first.name}")
+        }
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -43,10 +63,14 @@ fun TrackListScreen(
         ) {
             Text("Keine SOngs Ürbig wechsel zu liste")
 
-            swipeableTracks.reversed().forEach {
+            swipeableTracks.reversed().forEachIndexed { index, it ->
                 key(it.id) {
+                    val br = rememberSwipeCardState()
+                    if (index == swipeableTracks.lastIndex) {
+                        topSwipeCardState = br
+                    }
                     SwipeCard(
-                        swipeCardState = rememberSwipeCardState(),
+                        swipeCardState = br,
                         onSwiped = { dir ->
                             when (dir) {
                                 SwipeDirection.Left -> trackListViewModel.swipeLeft(it)
@@ -61,6 +85,13 @@ fun TrackListScreen(
                     }
                 }
             }
+
+            Button(onClick = {
+                scope.launch {
+
+                    topSwipeCardState?.swipe(SwipeDirection.Up)
+                }
+            }) { Text("swipe up") }
         }
     }
 
