0438be04b56dd0909fcbcd04d86486d9c883076a
jana-ja <jana.nein+account@posteo.de>
2025-04-26 20:51:15 +0200

    fix resource

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/ui/component/VoteButton.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/ui/component/VoteButton.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
