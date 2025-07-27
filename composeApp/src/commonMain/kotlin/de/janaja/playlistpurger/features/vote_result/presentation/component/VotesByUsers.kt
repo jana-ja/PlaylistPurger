c7e24e3922d56e09fc666bb5ea8bdb014fb24231
jana-ja <jana.nein+account@posteo.de>
2025-07-27 14:01:03 +0200

    show who voted how with the users thumbnail image in VotesByUsers component

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/vote_result/presentation/component/VotesByUsers.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/vote_result/presentation/component/VotesByUsers.kt
@@ -1,9 +1,13 @@
 package de.janaja.playlistpurger.features.vote_result.presentation.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -14,6 +18,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import de.janaja.playlistpurger.core.ui.component.CircleAsyncImage
 import de.janaja.playlistpurger.shared.domain.model.User
 import de.janaja.playlistpurger.shared.domain.model.VoteOption
 import org.jetbrains.compose.resources.painterResource
@@ -24,29 +29,32 @@ fun VotesByUsers(
     modifier: Modifier = Modifier
 ) {
 
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
 
         VoteOption.entries.forEach { vote ->
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
             ) {
                 Icon(
                     painter = painterResource(vote.resource),
                     contentDescription = vote.contentDescription,
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .scale(scaling),
-//                    tint = backgroundColor
+                    modifier = Modifier
+                        .size(24.dp)
                 )
+                Spacer(Modifier.width(4.dp))
                 usersByVoteOption[vote]?.forEach { user ->
-                    // TODO replace with image later
-                    Text(
-                        user?.name?.split(" ")?.joinToString { it.first().toString().uppercase() } ?: "UU",
-                        modifier = Modifier.size(24.dp).clip(CircleShape).background(Color.Cyan),
-                        textAlign = TextAlign.Center
-                    )
+                    user?.let { user ->
+                        CircleAsyncImage(
+                            url = user.thumbnailImage ?: "", // TODO
+                            contentDescription = user.name,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
                 }
-
             }
         }
     }
