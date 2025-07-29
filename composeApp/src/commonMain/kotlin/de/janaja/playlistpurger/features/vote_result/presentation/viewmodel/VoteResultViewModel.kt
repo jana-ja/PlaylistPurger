f1273c1117474d32a6cb54ee9d8658832f41b16c
jana-ja <jana.nein+account@posteo.de>
2025-07-29 21:19:46 +0200

    fix VoteResultViewModel StateFlow

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/vote_result/presentation/viewmodel/VoteResultViewModel.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/vote_result/presentation/viewmodel/VoteResultViewModel.kt
@@ -2,6 +2,7 @@ package de.janaja.playlistpurger.features.vote_result.presentation.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import de.janaja.playlistpurger.core.domain.exception.DataException
 import de.janaja.playlistpurger.core.ui.VoteResultRoute
@@ -9,7 +10,9 @@ import de.janaja.playlistpurger.core.ui.model.DataState
 import de.janaja.playlistpurger.core.ui.util.UiText
 import de.janaja.playlistpurger.core.ui.util.toStringResId
 import de.janaja.playlistpurger.features.vote_result.domain.usecase.GetTracksWithAllVotesUseCase
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import playlistpurger.composeapp.generated.resources.Res
 import playlistpurger.composeapp.generated.resources.generic_error_message
 
@@ -35,4 +38,9 @@ class VoteResultViewModel(
                 }
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = DataState.Loading
+        )
 }
