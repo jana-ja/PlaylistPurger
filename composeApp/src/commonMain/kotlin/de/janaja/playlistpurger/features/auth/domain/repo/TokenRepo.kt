1ef141ca2e2557ee6314e1ae723e120bed8dd46e
jana-ja <jana.nein+account@posteo.de>
2025-08-01 20:36:32 +0200

    remove todo

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/auth/domain/repo/TokenRepo.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/features/auth/domain/repo/TokenRepo.kt
@@ -2,11 +2,9 @@ package de.janaja.playlistpurger.features.auth.domain.repo
 
 import kotlinx.coroutines.flow.Flow
 
-// TODO maybe remove from domain
 interface TokenRepo {
     val accessTokenFlow: Flow<String?>
     val refreshTokenFlow: Flow<String?>
-//    val userIdFlow: Flow<String?>
     suspend fun updateAccessToken(token: String)
     suspend fun updateRefreshToken(token: String)
     suspend fun deleteAllToken()
