6b3d7fe1582f835341a5b624d5c53dcd4bb17be4
jana-ja <jana.nein+account@posteo.de>
2025-08-01 08:43:30 +0200

    add Log and clean up

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/core/domain/usecase/ExecuteAutheticatedRequestUseCase.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/core/domain/usecase/ExecuteAutheticatedRequestUseCase.kt
@@ -12,64 +12,42 @@ class ExecuteAuthenticatedRequestUseCase(
 ) {
     private val TAG = "ExecuteAuthenticatedRequestUseCase"
     suspend operator fun <T> invoke(request: suspend () -> Result<T>): Result<T> {
+        Log.d("ExecuteAuthenticatedRequestUseCase", "invoke")
         val result = request()
         result.fold(
             onSuccess = {
                 return Result.success(it)
             },
             onFailure = { e ->
-                if (e is DataException.Remote.InvalidAccessToken) {
+                when (e) {
+                    is DataException.Remote.InvalidAccessToken -> {
 
                         Log.i(TAG, "access token is invalid\ntry to refresh...")
 
-                    refreshTokenUseCase().fold(
-                        onSuccess = {
-                            // try again after refresh
-                            return request()
-                        },
-                        onFailure = {
-                            // TODO logout here?
-                            //  is logout handled automatically by checkToken in authService? should it really?
-                            //  handle logout from viewmodel?
-                            return Result.failure(it)
-                        }
-                    )
-                } else if (e is DataException.Auth) {
-                    logoutUseCase()
-                    return Result.failure(e)
+                        refreshTokenUseCase().fold(
+                            onSuccess = {
+                                // try again after refresh
+                                return request()
+                            },
+                            onFailure = {
+                                // TODO logout here?
+                                //  is logout handled automatically by checkToken in authService? should it really?
+                                //  handle logout from viewmodel?
+                                return Result.failure(it)
+                            }
+                        )
+                    }
+                    is DataException.Auth -> {
+                        logoutUseCase()
+                        return Result.failure(e)
 
-                } else {
-                    // TODO
-                    return Result.failure(DataException.Remote.Unknown)
+                    }
+                    else -> {
+                        // TODO
+                        return Result.failure(DataException.Remote.Unknown)
+                    }
                 }
             }
         )
     }
 }
-/*
-
-                    // lololo vllt an result extenden statt vm?
-                    resultToDataState(
-                        apiResult = playlistResult,
-                        successTransform = { lel ->
-
-                            return@resultToDataState lel.filter(filter.function)
-                        },
-                        // TODO doch move refresh and logout inside the function?
-                        onRefresh = {
-                            viewModelScope.launch {
-                                Log.i(TAG, "flow: try refresh token")
-                                if (authService.refreshToken()) {
-                                    Log.i(TAG, "flow: token refresh successfull")
-                                    // TODO test this
-                                    _retryTrigger.value = LocalDateTime.now()
-                                }
-                            }
-                        },
-                        onLogout = {
-                            viewModelScope.launch {
-                                authService.logout()
-                            }
-                        },
-                    )
- */
