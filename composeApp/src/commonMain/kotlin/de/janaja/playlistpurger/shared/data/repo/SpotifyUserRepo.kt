83bd23e098ad410bc870d29e700af24d83428d1f
jana-ja <jana.nein+account@posteo.de>
2025-08-02 09:59:41 +0200

    switch to lru cache for users

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/shared/data/repo/SpotifyUserRepo.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/shared/data/repo/SpotifyUserRepo.kt
@@ -1,7 +1,7 @@
 package de.janaja.playlistpurger.shared.data.repo
 
 import de.janaja.playlistpurger.core.domain.exception.DataException
-import de.janaja.playlistpurger.core.util.ConcurrentCache
+import de.janaja.playlistpurger.core.util.ConcurrentLruCache
 import de.janaja.playlistpurger.features.auth.domain.service.AuthService
 import de.janaja.playlistpurger.shared.data.model.toUser
 import de.janaja.playlistpurger.shared.data.remote.SpotifyWebApi
@@ -16,12 +16,9 @@ class SpotifyUserRepo(
 
     private val tokenFlow = authService.accessToken
 
-    // cache
-    // concurrent hash map
-    // TODO LRU (least recently used) mit number of items
     // no invalidation, user image could become stale but thats okay
     // no time to live (ttl)
-    private val userCache: MutableMap<String, UserDetails.Full> = ConcurrentCache()
+    private val userCache: MutableMap<String, UserDetails.Full> = ConcurrentLruCache(150)
 
     override suspend fun getUserForId(userId: String): Result<UserDetails.Full> {
         userCache[userId]?.let { cachedUser ->
