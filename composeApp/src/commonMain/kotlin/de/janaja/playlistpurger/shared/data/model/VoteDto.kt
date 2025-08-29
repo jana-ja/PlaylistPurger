21dbffe62c6f3c475dc9a15f9ea81e419a307ee1
jana-ja <jana.nein+account@posteo.de>
2025-08-29 23:22:35 +0200

    make VoteDto Serializable

--- composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/shared/data/model/VoteDto.kt
+++ composeApp/src/commonMain/kotlin/de/janaja/playlistpurger/shared/data/model/VoteDto.kt
@@ -1,7 +1,9 @@
 package de.janaja.playlistpurger.shared.data.model
 
 import de.janaja.playlistpurger.shared.domain.model.VoteOption
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class VoteDto(
     val playlistId: String,
     val trackId: String,
